a90782d9d47048d7ef5a7d27dc6f8b0c0c729b8d
ace <kyleguerrero85@gmail.com>
2023-01-07 17:11:56 -0600

    completed more challenges

--- activities/src/bin/a22.rs
+++ activities/src/bin/a22.rs
@@ -23,12 +23,37 @@ fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
 
 /// Divides a and b.
 fn div(a: i32, b: i32) -> Option<i32> {
+    if b == 0 {
+        return None;
+    }
+
     Some(a / b)
 }
 
 /// Takes two strings and places them immediately one after another.
 fn concat(first: &str, second: &str) -> String {
-    format!("{} {}", first, second)
+    format!("{}{}", first, second)
 }
 
 fn main() {}
+
+#[cfg(test)]
+mod test {
+    use crate::*; // the current file and all its contents
+
+    #[test]
+    fn divide_nums() {
+        let result = div(4, 2);
+        let expected = Some(2);
+
+        assert_eq!(result, expected, "Value should be a decimal");
+    }
+
+    #[test]
+    fn test_concat() {
+        let result = concat("cat", "dog");
+        let expect = "catdog".to_owned();
+
+        assert_eq!(result, expect, "Should be catdog");
+    }
+}
--- activities/src/bin/a26.rs
+++ activities/src/bin/a26.rs
@@ -8,4 +8,7 @@
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
 
-fn main() {}
+use chrono::prelude::*;
+fn main() {
+    let utc: DateTime<Utc> = Utc::new();
+}
